c733ad3be575ee1e558a3422b673d22154dd4601
Mohamed Afrid <meetafrid@gmail.com>
2021-09-27 08:24:27 +0530

    "dataStructure algo for the Array in rotation"

--- src/com/datastructure/RotateArray.java
+++ src/com/datastructure/RotateArray.java
@@ -26,8 +26,7 @@ class RotateArray {
     }
 
     /* utility function to print an array */
-    void printArray(int arr[], int n)
-    {
+    void printArray(int arr[], int n) {
         for (int i = 0; i < n; i++) {
             System.out.print(arr[i] + " ");}
     }
